e807a5f3ae2d6645b71f572b7c6076bcf6c66d13
gbs127 <gbsoares127@gmail.com>
2021-10-24 03:45:01 -0300

    Add Folder Structure Add @react-navigation/native

---
+++ src/theme/colors.js
@@ -0,0 +1,16 @@
+const colors = {
+    primary: '',
+    secondary: '',
+    textPrimary: '',
+    textTertiary: '',
+    textGrey: '',
+    borderColor: '',
+    backgroundColor: '',
+    ratingColor: '',
+    progressGrey: '',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#F9F9F9'
+}
+
+export default colors;
